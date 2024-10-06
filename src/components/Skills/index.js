27561bb0d4424a619f4fb16c8b57714c5fcd45a7
shaiksajidhussain <sanjusazid0@gmail.com>
2024-10-06 23:12:35 +0530

    Added s

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -215,7 +215,7 @@ const Skills = () => {
     <Container id="skills" ref={skillsRef}>
       <Wrapper>
         <Title className="skills-title">Skills</Title>
-        <Desc className="skills-desc">Here are some of my skills on which I have been working on for the past 2 years.</Desc>
+        <Desc className="skills-desc">Heree are some of my skills on which I have been working on for the past 2 years.</Desc>
         <SkillsContainer>
           {skills.map((skill, index) => (
             <Skill key={index} ref={el => skillCardsRef.current[index] = el}>
